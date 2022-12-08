7e8bfaa4727710454c2b91b2d96ea02dd28e0603
syed <syedyasirkamal@gmail.com>
2022-12-08 04:13:05 -0500

    fourth flask-appl deploy

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,7 +4,7 @@ set -xe
 
 # Copy war file from S3 bucket to tomcat webapp folder
 aws s3 cp s3://codedeploystack-webappdeploymentbucket-sjfjvgm9ghr6/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-sjfjvgm9ghr6/FlaskApplication.py /usr/local/tomcat9/webapps/FlaskApplication.py
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-sjfjvgm9ghr6/FlaskApplication.py /usr/local/tomcat9/webapps/FlaskApplication
 
 
 # Ensure the ownership permissions are correct.
